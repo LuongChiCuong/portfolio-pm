a398ec6b6e9c72b3ae8829e3c14145c4954c2b4d
LuongChiCuong <luongchicuong93@gmail.com>
2018-03-12 23:45:34 +0700

    update index

---
+++ public/javascript/home.js
@@ -0,0 +1,4 @@
+$(window).scroll(function (event) {
+  console.log('scroll active');
+  $('.greeting-content').addClass('hide-content');
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cuong - Product Manager' });
+  res.render('index', { title: 'Cuong Luong - Product Manager' });
 });
 
 module.exports = router;
