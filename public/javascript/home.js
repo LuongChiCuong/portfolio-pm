038d79f224b5355d0013e8a5aaf22867ff51ec74
Cuong-zenbook <elvis@mobiclixgroup.com>
2018-03-15 18:20:18 +0700

    update about page

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,5 +1,5 @@
 $(window).scroll(function (event) {
   console.log('scroll active');
-  window.location.href = window.location.href + "about";
+  // window.location.href = window.location.href + "about";
   // $('.greeting-content').addClass('hide-content');
 });
